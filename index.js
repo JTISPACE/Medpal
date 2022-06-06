7ac92d1656a0ea4fec3ffdc836fc19670734cc90
JTISPACE <99241250+JTISPACE@users.noreply.github.com>
2022-06-06 11:41:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-let slides = document.querySelectorAll(".slide");
-let cards = document.querySelectorAll(".row-card");
-console.log(cards);
-
-slideIndex = 0;
-
-// slideShow();
-
-function slideShow() {
-  let i;
-
-  for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
-
-  slideIndex++;
-
-  if (slideIndex > slides.length) slideIndex = 1;
-
-  for (i = 0; i < cards.length; i++) cards[i].style.display = "none";
-
-  slides[slideIndex - 1].style.display = "block";
-  cards[slideIndex - 1].style.display = "flex";
+var navLinks = document.getElementById("navLinks");
+function showmenu() {
+  navLinks.style.right = "0";
 }
-
-setInterval(slideShow, 7000);
+function hidemenu() {
+  navLinks.style.right = "-300px";
+}
