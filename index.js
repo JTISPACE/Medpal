614d70f500568fdaa2c7d9fbb9a9f27bc1a4354c
JTISPACE <99241250+JTISPACE@users.noreply.github.com>
2022-06-07 08:08:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function showmenu() {
 function hidemenu() {
   navLinks.style.right = "-300px";
 }
+
+
+var navLinks = document.getElementById("navLinks");
+function showmenu() {
+  navLinks.style.right = "0";
+}
+function hidemenu() {
+  navLinks.style.right = "-300px";
+}
+
+var checkerBox = document.getElementById("checkerBox");
+var chevRight = document.getElementById("chevRight");
+var bubble = document.getElementById("bubbleIcon");
+function showbox() {
+  checkerBox.style.right = "10px";
+  checkerBox.style.display = "flex";
+
+  bubble.style.display = "none";
+  checkerBox.style.position = "absolute";
+}
+function hidebox() {
+  checkerBox.style.display = "none";
+  bubble.style.display = "block";
+}
